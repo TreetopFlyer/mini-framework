3fcad3dfa9e7e6270314666a0d555591d353f0ab
unknown <TreetopFlyer>
2019-11-01 23:49:42 -0400

    send and draw arguments added to views

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
 import {render} from './node_modules/lit-html/lit-html.js';
 var App;
 App = {
-    _DOM:{},
+
     _Model:{},
     _Mutations:{},
     _Views:{},
     _View:{},
-    Update: (inEnum, inPayload)=>
+    _DOM:{},
+
+    Mutate: (inEnum, inPayload)=>
     {
         var mutation;
         mutation = App._Mutations[inEnum];
         if(mutation)
         {
-            console.log(inEnum, inPayload);
-            mutation(App._Model, inPayload);
+            return ()=>
+            {
+                mutation(App._Model, inPayload);
+                App.Render();
+            }
         }
-        render(App._View(App._Model, App.Update, App._Views), App._DOM);
+    },
+    Component:(inEnum, inPayload, inArray)=>
+    {
+        var view;
+        view = App._Views[inEnum];
+        if(view)
+        {
+            if(inPayload == null)
+            {
+                return inArray.map(item => view(item, App.Mutate, App.Component));
+            }
+            else
+            {
+                return view(inPayload, App.Mutate, App.Component);
+            }
+            
+        }
+    },
+    Render:()=>
+    {
+        render(App._View(App._Model, App.Mutate, App.Component), App._DOM);
     },
     Start:(inDOM, inView)=>
     {
         App._DOM = inDOM;
         App._View = App._Views[inView];
-        render(App._View(App._Model, App.Update, App._Views), App._DOM);
+        App.Render();
     }
 };
 export default App;
